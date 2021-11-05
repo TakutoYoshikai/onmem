b54e9c380465bc61e178e89f3e64410ac0f74cc0
lucky <takuto.yoshikai@gmail.com>
2021-11-05 14:18:31 +0900

    fix

--- keys.js
+++ keys.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const request = require("request");
 
 const host = process.argv[2];
--- post.js
+++ post.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const request = require("request");
 
 const input = require("fs").readFileSync("/dev/stdin");
